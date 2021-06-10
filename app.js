a5772a41db809b72c0e6a11feaf59af3612cc608
Mohib Khan <mohibk.in@gmail.com>
2021-06-10 15:35:40 +0530

    Added banana speak

---
+++ app.js
@@ -0,0 +1,24 @@
+const textInput = document.querySelector("#text-input");
+const textOutput = document.querySelector("#text-output");
+const btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", handleClick);
+
+serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?text=" + text;
+}
+
+function handleClick() {
+  fetch(getTranslationURL(textInput.value))
+    .then((res) => res.json())
+    .then((json) => {
+      textOutput.innerText = json.contents.translated;
+    })
+    .catch(() => handleError);
+}
+
+function handleError() {
+  alert("Something went wrong! Please try again after sometime.");
+}
